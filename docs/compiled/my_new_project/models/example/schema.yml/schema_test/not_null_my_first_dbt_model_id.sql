b34f070b245748bdeb210da70f429a0c83ee27b1
EzraBloemendaal <90531183+EzraBloemendaal@users.noreply.github.com>
2021-11-16 15:01:17 +0100

    Delete docs directory

--- docs/compiled/my_new_project/models/example/schema.yml/schema_test/not_null_my_first_dbt_model_id.sql
+++
@@ -1,9 +0,0 @@
-
-    
-    
-
-select *
-from laboratory_dev.raw.my_first_dbt_model
-where id is null
-
-
--- docs/compiled/my_new_project/models/example/schema.yml/schema_test/not_null_my_second_dbt_model_id.sql
+++
@@ -1,9 +0,0 @@
-
-    
-    
-
-select *
-from laboratory_dev.raw.my_second_dbt_model
-where id is null
-
-
